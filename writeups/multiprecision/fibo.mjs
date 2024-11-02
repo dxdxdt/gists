0bf2a61aed938df9a2dd5188818255c977d4a77b
David Timber <dxdt@dev.snart.me>
2024-11-02 22:33:52 +0100

    writeups/multiprecision: fix print error ...
    - Remove '--suppress-common-lines' option when producing diff

--- writeups/multiprecision/fibo.mjs
+++ writeups/multiprecision/fibo.mjs
@@ -1,37 +1,28 @@
 #!/bin/env node
 
-function do_fibo (num_f) {
+function do_fibo (num_f, str_f) {
 	let a, b, c;
+
+	if (typeof str_f === 'undefined') {
+		str_f = (n) => n.toString();
+	}
 
 	a = num_f('0');
 	b = num_f('1');
 
 	for (let i = 0; i < 101; i += 1) {
 		c = a + b;
-		console.info(c.toString());
+		console.info(str_f(c));
 		b = a;
 		a = c;
 	}
 }
 
-function parse_flag (s) {
-	s = s.toLowerCase();
+const flag = process.argv.length > 2 ? process.argv[2] : 0;
 
-	if (s === 'true') {
-		return true;
-	}
-	else if (s === 'false') {
-		return false;
-	}
-
-	return Number(s);
-}
-
-const flag = process.argv.length > 2 ? parse_flag(process.argv[2]) : true;
-
-if (flag) {
-	do_fibo(BigInt);
-}
-else {
-	do_fibo(Number);
+switch (flag) {
+case '0': do_fibo(Number); break;
+case '1': do_fibo(BigInt); break;
+case '2': do_fibo(Number, (n) => n.toFixed()); break;
+default: throw new Error(process.argv[1] + ": " + flag + ": unknown flag");
 }
